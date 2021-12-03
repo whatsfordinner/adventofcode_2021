7b06d28b289e090a3f9c7db958bc515af213e3df
what's for dinner <cruelangeleo7@gmail.com>
2021-12-03 22:31:20 +1100

    solved day 3

---
+++ day03/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"math"
+	"os"
+)
+
+func main() {
+	inputFile := os.Args[1]
+	input := getInput(inputFile)
+	gamma, epsilon := findGammaAndEpsilon(input)
+	log.Printf("Result: %d", binaryToDecimal(gamma)*binaryToDecimal(epsilon))
+	oxygen := findMeetsCriteria(0, input, pickMostCommon)
+	co2 := findMeetsCriteria(0, input, pickLeastCommon)
+	log.Printf("Result: %d", binaryToDecimal(&oxygen)*binaryToDecimal(&co2))
+}
+
+func getInput(filename string) *[][]int {
+	result := new([][]int)
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if scanner.Text() != "" {
+			newLine := new([]int)
+			for _, v := range scanner.Text() {
+				bit := int(v - '0')
+				*newLine = append(*newLine, bit)
+			}
+			*result = append(*result, *newLine)
+		}
+	}
+
+	return result
+}
+
+func findGammaAndEpsilon(in *[][]int) (*[]int, *[]int) {
+	gamma := new([]int)
+	epsilon := new([]int)
+	numRows := len(*in)
+	numCols := len((*in)[0])
+
+	for i := 0; i < numCols; i++ {
+		count := 0
+		for j := 0; j < numRows; j++ {
+			if (*in)[j][i] == 1 {
+				count++
+			}
+		}
+		if count > numRows/2 {
+			*gamma = append(*gamma, 1)
+			*epsilon = append(*epsilon, 0)
+		} else {
+			*gamma = append(*gamma, 0)
+			*epsilon = append(*epsilon, 1)
+		}
+	}
+
+	return gamma, epsilon
+}
+
+func binaryToDecimal(in *[]int) int {
+	result := 0
+
+	for i := 0; i < len(*in); i++ {
+		result += (*in)[len(*in)-i-1] * int(math.Pow(2, float64(i)))
+	}
+
+	return result
+}
+
+func findMeetsCriteria(index int, in *[][]int, decider func(*[][]int, *[][]int) *[][]int) []int {
+	if len(*in) == 1 {
+		return (*in)[0]
+	} else {
+		zeros := new([][]int)
+		ones := new([][]int)
+
+		for i := 0; i < len(*in); i++ {
+			if (*in)[i][index] == 1 {
+				*ones = append(*ones, (*in)[i])
+			} else {
+				*zeros = append(*zeros, (*in)[i])
+			}
+		}
+
+		return findMeetsCriteria(index+1, decider(ones, zeros), decider)
+	}
+}
+
+func pickMostCommon(ones *[][]int, zeros *[][]int) *[][]int {
+	if len(*ones) >= len(*zeros) {
+		return ones
+	}
+
+	return zeros
+}
+
+func pickLeastCommon(ones *[][]int, zeros *[][]int) *[][]int {
+	if len(*ones) < len(*zeros) {
+		return ones
+	}
+
+	return zeros
+}
